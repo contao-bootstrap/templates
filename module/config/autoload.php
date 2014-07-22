948ac4b675c81dff8a983b4f613d6f57bc7b8868
David Molineus <mail@netzmacht.de>
2014-07-22 07:34:24 +0200

    add breadcrumb template

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -4,6 +4,7 @@
 	'com_bootstrap' 	=> 'system/modules/bootstrap-templates/templates/generic',
 	'gallery_carousel'  => 'system/modules/bootstrap-templates/templates/generic',
 	'gallery_grid'      => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_breadcrumb'    => 'system/modules/bootstrap-templates/templates/generic',
 	'mod_comment_form'  => 'system/modules/bootstrap-templates/templates/generic',
 	'mod_quicklist'     => 'system/modules/bootstrap-templates/templates/generic',
 	'mod_quicknav'      => 'system/modules/bootstrap-templates/templates/generic',
