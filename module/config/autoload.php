e3e79836132aab0b6e3cf0848fa6e0b85f5f9acb
David Molineus <mail@netzmacht.de>
2014-07-22 08:07:33 +0200

    add search advanced template

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -1,12 +1,13 @@
 <?php
 
 \TemplateLoader::addFiles(array(
-	'com_bootstrap' 	=> 'system/modules/bootstrap-templates/templates/generic',
-	'gallery_carousel'  => 'system/modules/bootstrap-templates/templates/generic',
-	'gallery_grid'      => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_breadcrumb'    => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_comment_form'  => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_quicklist'     => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_quicknav'      => 'system/modules/bootstrap-templates/templates/generic',
-	'pagination'        => 'system/modules/bootstrap-templates/templates/generic',
+	'com_bootstrap'       => 'system/modules/bootstrap-templates/templates/generic',
+	'gallery_carousel'    => 'system/modules/bootstrap-templates/templates/generic',
+	'gallery_grid'        => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_breadcrumb'      => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_comment_form'    => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_search_advanced' => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_quicklist'       => 'system/modules/bootstrap-templates/templates/generic',
+	'mod_quicknav'        => 'system/modules/bootstrap-templates/templates/generic',
+	'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
 ));
