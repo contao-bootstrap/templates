6cc631e346aeeae1a534e462034edac6ce46edb5
David Molineus <david.molineus@netzmacht.de>
2015-01-26 15:53:48 +0100

    Remove quicklink and quicknav templates as they are part of contao-bootstrap/navigation now.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -16,8 +16,6 @@
 	'mod_breadcrumb'      => 'system/modules/bootstrap-templates/templates/generic',
 	'mod_comment_form'    => 'system/modules/bootstrap-templates/templates/generic',
 	'mod_search_advanced' => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_quicklist'       => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_quicknav'        => 'system/modules/bootstrap-templates/templates/generic',
 	'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
 	'picture_default'	  => 'system/modules/bootstrap-templates/templates/generic',
 ));
