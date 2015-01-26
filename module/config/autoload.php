07b91a0f4f0b10a0faa3018473981fc8244095f0
David Molineus <david.molineus@netzmacht.de>
2015-01-26 16:06:58 +0100

    Add pagination template for Contao 3.4

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -10,12 +10,18 @@
  */
 
 \TemplateLoader::addFiles(array(
-	'com_bootstrap'       => 'system/modules/bootstrap-templates/templates/generic',
-	'gallery_carousel'    => 'system/modules/bootstrap-templates/templates/generic',
-	'gallery_grid'        => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_breadcrumb'      => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_comment_form'    => 'system/modules/bootstrap-templates/templates/generic',
-	'mod_search_advanced' => 'system/modules/bootstrap-templates/templates/generic',
-	'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
-	'picture_default'	  => 'system/modules/bootstrap-templates/templates/generic',
+    'com_bootstrap'       => 'system/modules/bootstrap-templates/templates/generic',
+    'gallery_carousel'    => 'system/modules/bootstrap-templates/templates/generic',
+    'gallery_grid'        => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_breadcrumb'      => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_comment_form'    => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_search_advanced' => 'system/modules/bootstrap-templates/templates/generic',
+    'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
+    'picture_default'     => 'system/modules/bootstrap-templates/templates/generic',
+    'gallery_carousel_33' => 'system/modules/bootstrap-templates/templates/generic/3.3',
+    'gallery_grid_33'     => 'system/modules/bootstrap-templates/templates/generic/3.3',
+    'pagination_33'       => 'system/modules/bootstrap-templates/templates/generic/3.3',
+    'gallery_carousel_34' => 'system/modules/bootstrap-templates/templates/generic/3.4',
+    'gallery_grid_34'     => 'system/modules/bootstrap-templates/templates/generic/3.4',
+    'pagination_34'       => 'system/modules/bootstrap-templates/templates/generic/3.4',
 ));
