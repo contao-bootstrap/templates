eb38c1a389c8aaced34628f9d7728b1e68a94551
squareZ <squareZ@users.noreply.github.com>
2015-01-30 16:03:45 +0100

    added login_2cl.html5

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -19,6 +19,7 @@
     'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
     'picture_default'     => 'system/modules/bootstrap-templates/templates/generic',
     'login_1cl'           => 'system/modules/bootstrap-templates/templates/generic',
+    'login_2cl'           => 'system/modules/bootstrap-templates/templates/generic',
     'gallery_carousel_33' => 'system/modules/bootstrap-templates/templates/3.3',
     'gallery_grid_33'     => 'system/modules/bootstrap-templates/templates/3.3',
     'pagination_33'       => 'system/modules/bootstrap-templates/templates/3.3',
