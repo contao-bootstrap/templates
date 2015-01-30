718b8a83b7ec121fc360641cd24f6d660b98905c
David Molineus <david.molineus@netzmacht.de>
2015-01-30 18:16:13 +0100

    Fix autoload information and readd me as author.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -2,6 +2,7 @@
 
 /**
  * @package    contao-bootstrap
+ * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     squareZ <squareZ@users.noreply.github.com>
  * @copyright  2014-2015 netzmacht creative David Molineus
  * @license    LGPL 3.0
@@ -15,11 +16,13 @@
     'gallery_grid'        => 'system/modules/bootstrap-templates/templates/generic',
     'mod_breadcrumb'      => 'system/modules/bootstrap-templates/templates/generic',
     'mod_comment_form'    => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_login_1cl'       => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_login_2cl'       => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_logout_1cl'      => 'system/modules/bootstrap-templates/templates/generic',
+    'mod_logout_2cl'      => 'system/modules/bootstrap-templates/templates/generic',
     'mod_search_advanced' => 'system/modules/bootstrap-templates/templates/generic',
     'pagination'          => 'system/modules/bootstrap-templates/templates/generic',
     'picture_default'     => 'system/modules/bootstrap-templates/templates/generic',
-    'login_1cl'           => 'system/modules/bootstrap-templates/templates/generic',
-    'login_2cl'           => 'system/modules/bootstrap-templates/templates/generic',
     'gallery_carousel_33' => 'system/modules/bootstrap-templates/templates/3.3',
     'gallery_grid_33'     => 'system/modules/bootstrap-templates/templates/3.3',
     'pagination_33'       => 'system/modules/bootstrap-templates/templates/3.3',
