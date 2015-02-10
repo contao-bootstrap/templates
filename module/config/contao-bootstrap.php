d9b05e32bbd3609af56cc682a7429fdddddd0f26
David Molineus <david.molineus@netzmacht.de>
2015-02-10 18:48:55 +0100

    Indent by space, not by tabs.

--- module/config/contao-bootstrap.php
+++ module/config/contao-bootstrap.php
@@ -10,28 +10,28 @@
  */
 
 return array(
-	'templates' => array(
-		'modifiers' => array(
-			'callback_replace-image-classes' => array(
-				'type'      => 'callback',
-				'callback'  => array('Netzmacht\Bootstrap\Templates\Modifier', 'replaceImageClasses'),
-				'templates' => array('ce_*'),
-			),
+    'templates' => array(
+        'modifiers' => array(
+            'callback_replace-image-classes' => array(
+                'type'      => 'callback',
+                'callback'  => array('Netzmacht\Bootstrap\Templates\Modifier', 'replaceImageClasses'),
+                'templates' => array('ce_*'),
+            ),
 
-			'callback_replace-table-classes' => array(
-				'type'      => 'callback',
-				'callback'  => array('Netzmacht\Bootstrap\Templates\Modifier', 'replaceTableClasses'),
-				'templates' => array('ce_*'),
-			),
+            'callback_replace-table-classes' => array(
+                'type'      => 'callback',
+                'callback'  => array('Netzmacht\Bootstrap\Templates\Modifier', 'replaceTableClasses'),
+                'templates' => array('ce_*'),
+            ),
 
-			'replace_pagination-active-class' => array
-			(
-				'type'        => 'replace',
-				'key'         => 'items',
-				'search'      => '<li><span class="current">',
-				'replace'     => '<li class="active"><span>',
-				'templates'   => array('pagination'),
-			),
-		),
-	),
+            'replace_pagination-active-class' => array
+            (
+                'type'        => 'replace',
+                'key'         => 'items',
+                'search'      => '<li><span class="current">',
+                'replace'     => '<li class="active"><span>',
+                'templates'   => array('pagination'),
+            ),
+        ),
+    ),
 );
