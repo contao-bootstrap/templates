dc4f91ed6a47992d48d397fd42aa15c91e23d0cb
David Molineus <david.molineus@netzmacht.de>
2015-04-08 17:31:45 +0200

    Update translations.

---
+++ module/languages/de/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-04-08T17:30:55+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['0'] = 'Contao-Bootstrap Templates';
+$GLOBALS['TL_LANG']['MOD']['1'] = 'Contao-Bootstrap Template-Komponenten';
+
