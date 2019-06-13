a861b1ce621ddfa9432bcfebc5dc7391d4345c8d
David Molineus <david.molineus@netzmacht.de>
2019-06-13 17:09:07 +0200

    Support responsive-embed for youtube and vimeo videos

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,8 @@ Changelog
 
  - Provide newsletter template `nl_bs`
  - Button classes are insert at a tag for hyperlink elements (See #22)
+ - Add responsive-embed classes for youtube and vimeo player having aspect ratio ([#21](https://github.com/contao-bootstrap/templates/issues/21))
+ - Backport playerAspect for video players so that responsive-embed is usable in Contao 4.4 ([#21](https://github.com/contao-bootstrap/templates/issues/21))
 
 2.0.0 (2018-08-01)
 ------------------
---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Bootstrap templates.
+ *
+ * @package    contao-bootstrap
+ * @subpackage Templates
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2019 netzmacht David Molineus. All rights reserved.
+ * @license    https://github.com/contao-bootstrap/templates/blob/master/LICENSE LGPL 3.0-or-later
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+
+$GLOBALS['TL_LANG']['tl_content']['playerAspect'][0] = 'Aspect ratio';
+$GLOBALS['TL_LANG']['tl_content']['playerAspect'][1] = 'Here you can select the aspect ratio of the video to make it responsive.';
