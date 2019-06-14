a07e264f7cc11e799a9719de1acf5170750ce8f0
David Molineus <david.molineus@netzmacht.de>
2019-06-14 09:11:36 +0200

    Add german translation

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2019-06-14T07:11:00+00:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_content']['playerAspect']['0'] = 'Seitenverhältnis';
+$GLOBALS['TL_LANG']['tl_content']['playerAspect']['1'] = 'Hier können Sie das Seitenverhältnis des Videos festlegen, um es responsiv darstellen zu können.';
+
