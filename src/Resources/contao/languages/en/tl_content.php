ba62dec7975a6a5845beb7823573f988ef5e1468
David Molineus <david.molineus@netzmacht.de>
2022-11-15 09:28:43 +0100

    Remove player aspect backport

--- src/Resources/contao/languages/en/tl_content.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$GLOBALS['TL_LANG']['tl_content']['playerAspect'][0] = 'Aspect ratio';
-$GLOBALS['TL_LANG']['tl_content']['playerAspect'][1] = 'Here you can select the aspect ratio of the video to make it responsive.';
