72cc08efa17d8256a3f071a2c15f5fb923849974
David Molineus <david.molineus@netzmacht.de>
2023-06-13 16:13:16 +0200

    Remove layout specific twig functions

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -4,23 +4,13 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Templates\Twig;
 
-use Contao\StringUtil;
-use ContaoBootstrap\Core\Environment;
 use ContaoBootstrap\Templates\View\Spacing;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
-use function array_unique;
-use function array_unshift;
-use function array_values;
-use function assert;
-use function implode;
-use function is_string;
-use function str_starts_with;
-
 final class TwigExtension extends AbstractExtension
 {
-    public function __construct(private readonly Environment $environment, private readonly Spacing $spacing)
+    public function __construct(private readonly Spacing $spacing)
     {
     }
 
@@ -28,97 +18,8 @@ final class TwigExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction(
-                'contao_bootstrap_extract_btn_classes',
-                [$this, 'extractBtnClasses'],
-                ['needs_context' => true],
-            ),
-            new TwigFunction(
-                'contao_bootstrap_extract_img_classes',
-                [$this, 'extractImgClasses'],
-                ['needs_context' => true],
-            ),
-            new TwigFunction(
-                'contao_bootstrap_enabled',
-                fn () => $this->environment->enabled,
-            ),
-            new TwigFunction(
-                'contao_bootstrap_margin',
-                [$this->spacing, 'margin'],
-            ),
-            new TwigFunction(
-                'contao_bootstrap_padding',
-                [$this->spacing, 'padding'],
-            ),
+            new TwigFunction('contao_bootstrap_margin', [$this->spacing, 'margin']),
+            new TwigFunction('contao_bootstrap_padding', [$this->spacing, 'padding']),
         ];
-    }
-
-    /**
-     * @param array<string,mixed> $context
-     *
-     * @return list<string>
-     *
-     * @psalm-suppress InvalidReturnType
-     * @psalm-suppress InvalidReturnStatement
-     */
-    public function extractBtnClasses(array &$context): array
-    {
-        $cssClasses = StringUtil::trimsplit(' ', $context['element_css_classes'] ?? '');
-        $btnClasses = [];
-
-        foreach ($cssClasses as $index => $cssClass) {
-            assert(is_string($cssClass));
-
-            if (str_starts_with($cssClass, 'btn-')) {
-                $btnClasses[] = $cssClass;
-                unset($cssClasses[$index]);
-            } elseif ($cssClass === 'btn') {
-                $btnClasses[] = $cssClass;
-                unset($cssClasses[$index]);
-            }
-        }
-
-        if ($btnClasses === []) {
-            return [];
-        }
-
-        array_unshift($btnClasses, 'btn');
-
-        $context['element_css_classes'] = implode(' ', $cssClasses);
-
-        return array_values(array_unique($btnClasses));
-    }
-
-    /**
-     * @param array<string,mixed> $context
-     *
-     * @return list<string>
-     *
-     * @psalm-suppress InvalidReturnType
-     * @psalm-suppress InvalidReturnStatement
-     */
-    public function extractImgClasses(array &$context): array
-    {
-        $cssClasses = StringUtil::trimsplit(' ', $context['element_css_classes'] ?? '');
-        $imgClasses = [];
-
-        foreach ($cssClasses as $index => $cssClass) {
-            assert(is_string($cssClass));
-
-            if (! str_starts_with($cssClass, 'img-')) {
-                continue;
-            }
-
-            $imgClasses[] = $cssClass;
-            unset($cssClasses[$index]);
-        }
-
-        if ($imgClasses === []) {
-            return [];
-        }
-
-        $context['element_css_classes'] = implode(' ', $cssClasses);
-
-        return array_values(array_unique($imgClasses));
     }
 }
