f7ab1a382686011aa87d76da9ddbc19b81d51995
David Molineus <david.molineus@netzmacht.de>
2025-12-05 14:04:28 +0100

    Update tool configurations and fix new detected issues

--- src/Resources/contao/languages/de/tl_module.php
+++ src/Resources/contao/languages/de/tl_module.php
@@ -1,17 +1,6 @@
 <?php
-/**
- * Translations are managed using Transifex. To create a new translation
- * or to help to maintain an existing one, please register at transifex.com.
- *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2019-06-14T07:11:00+00:00
- */
 
+declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['tl_module']['bs_nav_class']['0'] = 'Bootstrap-Navigationsklassen';
 $GLOBALS['TL_LANG']['tl_module']['bs_nav_class']['1'] = 'Bitte legen Sie die Bootstrap-Navigationsklassen fest, die dem  <em>.nav</em>-Rootelement hinzugefügt werden sollen.';
-
--- src/Resources/contao/languages/de/tl_theme.php
+++ src/Resources/contao/languages/de/tl_theme.php
@@ -1,16 +1,6 @@
 <?php
-/**
- * Translations are managed using Transifex. To create a new translation
- * or to help to maintain an existing one, please register at transifex.com.
- *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2019-06-14T07:11:01+00:00
- */
 
+declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['tl_theme']['bs_gravatar_default']['0']          = 'Standard-Gravatar';
 $GLOBALS['TL_LANG']['tl_theme']['bs_gravatar_default']['1']          = 'Sie können einen Standard-Gravatar festlegen, der benutzt wird, falls ein Benutzer keinen Gravatar besitzt.';
@@ -18,4 +8,3 @@ $GLOBALS['TL_LANG']['tl_theme']['bs_gravatar_size']['0']             = 'Gravatar
 $GLOBALS['TL_LANG']['tl_theme']['bs_gravatar_size']['1']             = 'Größe des Gravatars in Pixeln';
 $GLOBALS['TL_LANG']['tl_theme']['bs_templates_disable_mapping']['0'] = 'Automatisches Template-Mapping deaktivieren';
 $GLOBALS['TL_LANG']['tl_theme']['bs_templates_disable_mapping']['1'] = 'Falls ausgewählt wird das automatische Mapping zwischen den Standard-Templates und den Bootstrap-Templates ausgeschaltet.';
-
